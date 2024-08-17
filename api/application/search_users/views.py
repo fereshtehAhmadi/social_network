0adb698313e5e1e3cc3ac0f60c2a9dd3378d2a98
fereshtehahmadi <freshtehahmadi01@gmail.com>
2024-08-17 12:14:42 +0330

    debug user profile api

--- api/application/search_users/views.py
+++ api/application/search_users/views.py
@@ -9,6 +9,7 @@ from rest_framework.response import Response
 from api.application.search_users.filters import CustomerProfilesFilter
 from api.application.search_users.serializers import AppUserListSerializer, AppUserProfileSerializer
 from apps.profiles.models import CustomerProfile
+from pagination import StandardPagination
 
 from tools.project.common.constants.cons import manualParametersDictCons
 from tools.project.common.helper_func import FactoryGetObject
@@ -36,6 +37,7 @@ get_swagger_kwargs = SwaggerAutoSchemaKwargs(
 
 class AppSearchUsersAPI(viewsets.ViewSet):
     permission_classes = [IsAuthenticated]
+    pagination_classes = StandardPagination
 
     serializers_dict = dict(
         GET=dict(
@@ -67,8 +69,18 @@ class AppSearchUsersAPI(viewsets.ViewSet):
         """
         display users list
         """
-        customers = CustomerProfilesFilter(request.GET).qs.filter(is_active=True).order_by('id').exclude(user=request.user)
-        return Response(AppUserListSerializer(customers, many=True).data)
+        customers = CustomerProfilesFilter(request.GET).qs.filter(is_active=True).order_by('id').exclude(
+            user=request.user)
+        class_page = self.pagination_classes()
+        page = class_page.paginate_queryset(
+            queryset=customers
+            , request=request
+        )
+        response = class_page.get_paginated_response(
+            data=AppUserListSerializer(page, many=True).data
+        ).data
+
+        return Response(response)
 
     @swagger_auto_schema(
         **get_swagger_kwargs(
@@ -85,4 +97,3 @@ class AppSearchUsersAPI(viewsets.ViewSet):
         customer = CustomerProfile.objects.get(pk=kwargs.get('pk'))
         serializer = AppUserProfileSerializer(customer, context={'user': request.user})
         return Response(serializer.data)
-
